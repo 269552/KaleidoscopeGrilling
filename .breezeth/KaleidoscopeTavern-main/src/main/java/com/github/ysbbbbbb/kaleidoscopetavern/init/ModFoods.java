6b6c5458274086d493e86b524f316b2f807fff71
breezeth <260703320+breezeth-CN@users.noreply.github.com>
2026-07-21 14:56:53 +0800

    0.8.0

--- .breezeth/KaleidoscopeTavern-main/src/main/java/com/github/ysbbbbbb/kaleidoscopetavern/init/ModFoods.java
+++
@@ -1,9 +0,0 @@
-package com.github.ysbbbbbb.kaleidoscopetavern.init;
-
-import net.minecraft.world.food.FoodProperties;
-
-public interface ModFoods {
-    FoodProperties GRAPE = (new FoodProperties.Builder())
-            .nutrition(2).saturationMod(0.5F)
-            .alwaysEat().build();
-}
