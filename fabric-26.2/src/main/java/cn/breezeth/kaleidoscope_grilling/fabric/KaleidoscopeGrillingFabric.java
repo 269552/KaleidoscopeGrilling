3b99a58116ba81fe99c0de8aa46ffbfa4097f5de
269552 <112224543+269552@users.noreply.github.com>
2026-09-18 22:20:32 +0800

    Add Fabric entrypoint

---
+++ fabric-26.2/src/main/java/cn/breezeth/kaleidoscope_grilling/fabric/KaleidoscopeGrillingFabric.java
@@ -0,0 +1,10 @@
+package cn.breezeth.kaleidoscope_grilling.fabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public final class KaleidoscopeGrillingFabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        // Port bootstrap will be filled in as NeoForge registrations/events are migrated.
+    }
+}
