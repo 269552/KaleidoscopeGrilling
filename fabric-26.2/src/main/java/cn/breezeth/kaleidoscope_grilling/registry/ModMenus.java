45030f96590559872138cc4b1dbc0e511179b64a
269552 <112224543+269552@users.noreply.github.com>
2026-09-19 10:51:53 +0800

    Port menu registry to Fabric 26.2

--- fabric-26.2/src/main/java/cn/breezeth/kaleidoscope_grilling/registry/ModMenus.java
+++ fabric-26.2/src/main/java/cn/breezeth/kaleidoscope_grilling/registry/ModMenus.java
@@ -1,29 +1,35 @@
 package cn.breezeth.kaleidoscope_grilling.registry;
 
 import cn.breezeth.kaleidoscope_grilling.KaleidoscopeGrilling;
-
+import cn.breezeth.kaleidoscope_grilling.fabric.registry.RegistryRef;
 import cn.breezeth.kaleidoscope_grilling.rack.AdvancedRackMenu;
 import cn.breezeth.kaleidoscope_grilling.rack.RackSelectionMenu;
 import cn.breezeth.kaleidoscope_grilling.rack.RackShortcutMenu;
-
-
+import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.MenuType;
-import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredRegister;
 
 public final class ModMenus {
-  public static final DeferredRegister<MenuType<?>> MENUS =
-      DeferredRegister.create(BuiltInRegistries.MENU, KaleidoscopeGrilling.MOD_ID);
-  public static final DeferredHolder<MenuType<?>, MenuType<AdvancedRackMenu>> ADVANCED_RACK =
-      MENUS.register(
-          "advanced_rack", () -> new MenuType<>(AdvancedRackMenu::new, FeatureFlags.VANILLA_SET));
-  public static final DeferredHolder<MenuType<?>, MenuType<RackSelectionMenu>> RACK_SELECTION =
-      MENUS.register(
-          "rack_selection", () -> new MenuType<>(RackSelectionMenu::new, FeatureFlags.VANILLA_SET));
-  public static final DeferredHolder<MenuType<?>, MenuType<RackShortcutMenu>> RACK_SHORTCUT =
-      MENUS.register(
-          "rack_shortcut", () -> new MenuType<>(RackShortcutMenu::new, FeatureFlags.VANILLA_SET));
+  public static final RegistryRef<MenuType<AdvancedRackMenu>> ADVANCED_RACK =
+      register("advanced_rack", new MenuType<>(AdvancedRackMenu::new, FeatureFlags.VANILLA_SET));
+  public static final RegistryRef<MenuType<RackSelectionMenu>> RACK_SELECTION =
+      register("rack_selection", new MenuType<>(RackSelectionMenu::new, FeatureFlags.VANILLA_SET));
+  public static final RegistryRef<MenuType<RackShortcutMenu>> RACK_SHORTCUT =
+      register("rack_shortcut", new MenuType<>(RackShortcutMenu::new, FeatureFlags.VANILLA_SET));
+
+  private static <T extends AbstractContainerMenu> RegistryRef<MenuType<T>> register(
+      String name, MenuType<T> type) {
+    Identifier id = Identifier.fromNamespaceAndPath(KaleidoscopeGrilling.MOD_ID, name);
+    ResourceKey<MenuType<?>> key = ResourceKey.create(Registries.MENU, id);
+    return RegistryRef.of(Registry.register(BuiltInRegistries.MENU, key, type));
+  }
+
+  public static void init() {}
+
   private ModMenus() {}
 }
