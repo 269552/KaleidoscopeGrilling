7e448d86c631e2558e8ce32ecaf52a1e6764aaf0
269552 <112224543+269552@users.noreply.github.com>
2026-09-19 10:50:29 +0800

    Port mob effect registration to Fabric 26.2

--- fabric-26.2/src/main/java/cn/breezeth/kaleidoscope_grilling/registry/ModEffects.java
+++ fabric-26.2/src/main/java/cn/breezeth/kaleidoscope_grilling/registry/ModEffects.java
@@ -1,34 +1,35 @@
 package cn.breezeth.kaleidoscope_grilling.registry;
 
 import cn.breezeth.kaleidoscope_grilling.KaleidoscopeGrilling;
-
 import cn.breezeth.kaleidoscope_grilling.effect.GrillingMobEffect;
-
-
+import cn.breezeth.kaleidoscope_grilling.fabric.registry.RegistryRef;
+import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
-import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredRegister;
 
 public final class ModEffects {
-  public static final DeferredRegister<MobEffect> EFFECTS =
-      DeferredRegister.create(BuiltInRegistries.MOB_EFFECT, KaleidoscopeGrilling.MOD_ID);
-  public static final DeferredHolder<MobEffect, MobEffect> HEAVY_METAL =
-      EFFECTS.register(
-          "heavy_metal", () -> new GrillingMobEffect(MobEffectCategory.BENEFICIAL, 0x70757D));
-  public static final DeferredHolder<MobEffect, MobEffect> HEAVY_METAL_POISONING =
-      EFFECTS.register(
-          "heavy_metal_poisoning",
-          () -> new GrillingMobEffect(MobEffectCategory.HARMFUL, 0x4F555D, false));
-  public static final DeferredHolder<MobEffect, MobEffect> DRAGON_BLOOD =
-      EFFECTS.register(
-          "dragon_blood", () -> new GrillingMobEffect(MobEffectCategory.BENEFICIAL, 0x7D1738));
-  public static final DeferredHolder<MobEffect, MobEffect> NUMB =
-      EFFECTS.register("numb", () -> new GrillingMobEffect(MobEffectCategory.NEUTRAL, 0xD8A22A));
-  public static final DeferredHolder<MobEffect, MobEffect> INVINCIBLE =
-      EFFECTS.register(
-          "invincible", () -> new GrillingMobEffect(MobEffectCategory.BENEFICIAL, 0xFFD44A));
+  public static final RegistryRef<MobEffect> HEAVY_METAL =
+      register("heavy_metal", new GrillingMobEffect(MobEffectCategory.BENEFICIAL, 0x70757D));
+  public static final RegistryRef<MobEffect> HEAVY_METAL_POISONING =
+      register("heavy_metal_poisoning", new GrillingMobEffect(MobEffectCategory.HARMFUL, 0x4F555D, false));
+  public static final RegistryRef<MobEffect> DRAGON_BLOOD =
+      register("dragon_blood", new GrillingMobEffect(MobEffectCategory.BENEFICIAL, 0x7D1738));
+  public static final RegistryRef<MobEffect> NUMB =
+      register("numb", new GrillingMobEffect(MobEffectCategory.NEUTRAL, 0xD8A22A));
+  public static final RegistryRef<MobEffect> INVINCIBLE =
+      register("invincible", new GrillingMobEffect(MobEffectCategory.BENEFICIAL, 0xFFD44A));
+
+  private static RegistryRef<MobEffect> register(String name, MobEffect effect) {
+    Identifier id = Identifier.fromNamespaceAndPath(KaleidoscopeGrilling.MOD_ID, name);
+    ResourceKey<MobEffect> key = ResourceKey.create(Registries.MOB_EFFECT, id);
+    return RegistryRef.of(Registry.register(BuiltInRegistries.MOB_EFFECT, key, effect));
+  }
+
+  public static void init() {}
 
   private ModEffects() {}
 }
