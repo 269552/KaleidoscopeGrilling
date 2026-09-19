378cc1e68b791bfd48537356034f89420574fd15
269552 <112224543+269552@users.noreply.github.com>
2026-09-19 10:49:30 +0800

    Port sound registration to Fabric 26.2

--- fabric-26.2/src/main/java/cn/breezeth/kaleidoscope_grilling/registry/ModSounds.java
+++ fabric-26.2/src/main/java/cn/breezeth/kaleidoscope_grilling/registry/ModSounds.java
@@ -1,43 +1,38 @@
 package cn.breezeth.kaleidoscope_grilling.registry;
 
 import cn.breezeth.kaleidoscope_grilling.KaleidoscopeGrilling;
-
+import cn.breezeth.kaleidoscope_grilling.fabric.registry.RegistryRef;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.sounds.SoundEvent;
-import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredRegister;
 
 public final class ModSounds {
-  public static final DeferredRegister<SoundEvent> SOUNDS =
-      DeferredRegister.create(Registries.SOUND_EVENT, KaleidoscopeGrilling.MOD_ID);
-  public static final DeferredHolder<SoundEvent, SoundEvent> ACTION_SUCCESS =
-      register("action_success");
-  public static final DeferredHolder<SoundEvent, SoundEvent> GRILL_FLIP = register("grill_flip");
-  public static final DeferredHolder<SoundEvent, SoundEvent> GRILL_LOOP = register("grill_loop");
-  public static final DeferredHolder<SoundEvent, SoundEvent> PICKUP_ITEM = register("pickup_item");
-  public static final DeferredHolder<SoundEvent, SoundEvent> SKEWER_DISASSEMBLE =
-      register("skewer_disassemble");
-  public static final DeferredHolder<SoundEvent, SoundEvent> SEASON = register("season");
-  public static final DeferredHolder<SoundEvent, SoundEvent> SHAKE_SEASONING =
-      register("shake_seasoning");
-  public static final DeferredHolder<SoundEvent, SoundEvent> SEASONING_BOTTLE_PLACE =
-      register("seasoning_bottle_place");
-  public static final DeferredHolder<SoundEvent, SoundEvent> SEASONING_BOTTLE_STACK =
-      register("seasoning_bottle_stack");
-  public static final DeferredHolder<SoundEvent, SoundEvent> ONE_SKEWER_EAT =
-      register("one_skewer_eat");
-  public static final DeferredHolder<SoundEvent, SoundEvent> TWO_SKEWER_EAT =
-      register("two_skewer_eat");
-  public static final DeferredHolder<SoundEvent, SoundEvent> THREE_SKEWER_EAT =
-      register("three_skewer_eat");
-  public static final DeferredHolder<SoundEvent, SoundEvent> FOUR_SKEWER_EAT =
-      register("four_skewer_eat");
+  public static final RegistryRef<SoundEvent> ACTION_SUCCESS = register("action_success");
+  public static final RegistryRef<SoundEvent> GRILL_FLIP = register("grill_flip");
+  public static final RegistryRef<SoundEvent> GRILL_LOOP = register("grill_loop");
+  public static final RegistryRef<SoundEvent> PICKUP_ITEM = register("pickup_item");
+  public static final RegistryRef<SoundEvent> SKEWER_DISASSEMBLE = register("skewer_disassemble");
+  public static final RegistryRef<SoundEvent> SEASON = register("season");
+  public static final RegistryRef<SoundEvent> SHAKE_SEASONING = register("shake_seasoning");
+  public static final RegistryRef<SoundEvent> SEASONING_BOTTLE_PLACE = register("seasoning_bottle_place");
+  public static final RegistryRef<SoundEvent> SEASONING_BOTTLE_STACK = register("seasoning_bottle_stack");
+  public static final RegistryRef<SoundEvent> ONE_SKEWER_EAT = register("one_skewer_eat");
+  public static final RegistryRef<SoundEvent> TWO_SKEWER_EAT = register("two_skewer_eat");
+  public static final RegistryRef<SoundEvent> THREE_SKEWER_EAT = register("three_skewer_eat");
+  public static final RegistryRef<SoundEvent> FOUR_SKEWER_EAT = register("four_skewer_eat");
 
-  private static DeferredHolder<SoundEvent, SoundEvent> register(String name) {
-    ResourceLocation id = ResourceLocation.fromNamespaceAndPath(KaleidoscopeGrilling.MOD_ID, name);
-    return SOUNDS.register(name, () -> SoundEvent.createVariableRangeEvent(id));
+  private static RegistryRef<SoundEvent> register(String name) {
+    Identifier id = Identifier.fromNamespaceAndPath(KaleidoscopeGrilling.MOD_ID, name);
+    ResourceKey<SoundEvent> key = ResourceKey.create(Registries.SOUND_EVENT, id);
+    SoundEvent sound = SoundEvent.createVariableRangeEvent(id);
+    return RegistryRef.of(Registry.register(BuiltInRegistries.SOUND_EVENT, key, sound));
   }
+
+  /** Forces class initialization from the Fabric entrypoint. */
+  public static void init() {}
 
   private ModSounds() {}
 }
